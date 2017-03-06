81bbbcce3100d24ed4e273400f41a4d47bcafa73
Ori Bernstein <ori@eigenstate.org>
2017-03-06 09:36:30 -0800

    Update to oh-god-im-incompetent release.

--- myrddin.rb
+++ myrddin.rb
@@ -1,8 +1,8 @@
 class Myrddin < Formula
   desc "Myrddin Programming Language"
   homepage "https://myrlang.org"
-  url "https://myrlang.org/releases/myrddin-0.1.tar.gz"
-  sha256 "55ac615a619beadb415356aa6244036390871b1c0954e2e8b40163866c013296"
+  url "https://myrlang.org/releases/myrddin-0.1.1.tar.gz"
+  sha256 "8d15ccdec8a1c60d1b7174903a7d0f4d7f80053614843375479e19426394024a"
 
   def install
     system "./configure", "--prefix=#{prefix}"
